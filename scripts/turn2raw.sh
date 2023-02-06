78966ae27112efad4b276974935f5fcac9e464d8
Bert Van Nuffelen <bert.vannuffelen@gmail.com>
2023-02-06 10:19:40 +0000

    release 3.0.5

--- scripts/turn2raw.sh
+++ scripts/turn2raw.sh
@@ -3,4 +3,4 @@
 FILE=$1
 COMMIT=$2
 
-jq --arg branchtag "$COMMIT"  -r ' .[] |=  . + {"type": "raw"} + {"directory" : .urlref} + {"repository":"https://github.com/SEMICeu/uri.semic.eu-generated" } + {"branchtag" : "\($branchtag)" } ' ${FILE}
+jq --arg branchtag "$COMMIT"  -r ' .[] |=  . + {"type": "raw"} + {"directory" : .urlref} + {"repository":"https://github.com/Informatievlaanderen/OSLO-Generated" } + {"branchtag" : "\($branchtag)" } ' ${FILE}
